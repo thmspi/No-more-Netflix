8aa4ae7ff92fa79e4b157f05653e742c3b0a502b
thomas <amiscremail@gmail.com>
2025-12-14 16:49:09 +0100

    update docker compose plugin install

--- ubuntu-server-setup.sh
+++ ubuntu-server-setup.sh
@@ -34,9 +34,18 @@ sudo apt upgrade -y
 log "Installing packages"
 sudo apt install -y \
   docker.io \
-  docker-compose-plugin \
   tree \
   cifs-utils
+
+log "Installing Docker Compose plugin"
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-compose-plugin
+docker compose version
 
 log "Enabling and starting Docker"
 sudo systemctl enable --now docker
